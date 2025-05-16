8b55ba9aeb0257c8f3318a870c5ca7a8471ef520
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-05-16 15:05:22 +0200

    EncryptUtil vervollständigt

--- tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/util/EncryptUtil.java
+++ tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/util/EncryptUtil.java
@@ -1,6 +1,18 @@
 package ch.bbw.pr.tresorbackend.util;
 
-import org.jasypt.util.text.AES256TextEncryptor;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Objects;
 
 /**
  * EncryptUtil
@@ -10,19 +22,41 @@ import org.jasypt.util.text.AES256TextEncryptor;
  */
 public class EncryptUtil {
 
-   //todo ergänzen!
+   private final String masterKey;
 
-   public EncryptUtil(String secretKey) {
-      //todo ergänzen!
+   public EncryptUtil(String masterKey) {
+      this.masterKey = masterKey;
    }
 
-   public String encrypt(String data) {
-      //todo anpassen!
-      return data;
+   public String encrypt(String data) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+      Cipher cipher = Cipher.getInstance("RSA");
+      cipher.init(Cipher.ENCRYPT_MODE, getPublicKey());
+
+      byte[] bytes = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+      return new String(Base64.getEncoder().encode(bytes));
    }
 
-   public String decrypt(String data) {
-      //todo anpassen!
-      return data;
+   public String decrypt(String data) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+      Cipher cipher = Cipher.getInstance("RSA");
+      cipher.init(Cipher.DECRYPT_MODE, getPrivateKey());
+
+      byte[] bytes = cipher.doFinal(Base64.getDecoder().decode(data));
+      return new String(bytes);
+   }
+
+   private PublicKey getPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+      byte[] publicKeyBytes = Objects.requireNonNull(getClass().getResourceAsStream("/key.pub")).readAllBytes();
+      var publicKeyFactory = KeyFactory.getInstance("RSA");
+      var publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+      return publicKeyFactory.generatePublic(publicKeySpec);
+   }
+
+   private PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+      byte[] privateKeyBytes = masterKey.getBytes(StandardCharsets.UTF_8);
+      KeyFactory privateKeyFactory = KeyFactory.getInstance("RSA");
+      EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+      return privateKeyFactory.generatePrivate(privateKeySpec);
    }
 }
