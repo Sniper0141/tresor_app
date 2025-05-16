1e3433932afd86affe2d118f123aeb803f5c4660
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-05-16 11:02:48 +0200

    getmasterkey Methode

--- tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/model/ConfigProperties.java
+++ tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/model/ConfigProperties.java
@@ -3,6 +3,9 @@ package ch.bbw.pr.tresorbackend.model;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * ConfigProperties
  * @author Peter Rutschmann
@@ -16,4 +19,17 @@ public class ConfigProperties {
    public String getOrigin() {
       return crossOrigin;
    }
+
+    public String getMasterKey() throws IOException {
+      var secretFile = new File("C:\\Development\\Schule\\Modul_183\\secrets\\tresor_app\\tresor_app_master_key.txt");
+      var scanner = new Scanner(secretFile);
+
+      StringBuilder result = new StringBuilder();
+      while (scanner.hasNextLine()) {
+         var line = scanner.nextLine();
+         result.append(line).append("\n");
+      }
+
+      return result.toString();
+    }
 }
