0bff7953e43a2c7378a53f454fef4168377ab76d
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-05-16 16:27:24 +0200

    Fix: korrekter filepath private-key

--- tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/MasterKeyService.java
+++ tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/MasterKeyService.java
@@ -1,13 +1,9 @@
 package ch.bbw.pr.tresorbackend.service;
 
-import ch.bbw.pr.tresorbackend.model.ConfigProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
 
 @Service
@@ -15,7 +11,7 @@ public class MasterKeyService {
 
 
     public String getMasterKey() throws FileNotFoundException {
-        var secretFile = new File("C:\\Development\\Schule\\Modul_183\\secrets\\tresor_app\\tresor_app_master_key.txt");
+        var secretFile = new File("C:\\Development\\Schule\\Modul_183\\secrets\\tresor_app_master_key.txt");
 
         Scanner scanner;
         scanner = new Scanner(secretFile);
