754ae0452d27f7730b666220735d1c1aedd7c45e
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2025-06-30 21:26:52 +0200

    unbenutzte usings weg

--- tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/model/ConfigProperties.java
+++ tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/model/ConfigProperties.java
@@ -1,7 +1,5 @@
 package ch.bbw.pr.tresorbackend.model;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
