9932b4916c2656af1157d2572db2961d270dddcf
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2025-07-11 11:54:05 +0200

    improve logging in password encryption script

--- tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/script/OneTimeEncryptionScript.java
+++ tresorbackend_rupe-master/183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/script/OneTimeEncryptionScript.java
@@ -1,13 +1,17 @@
 package ch.bbw.pr.tresorbackend.script;
 
+import ch.bbw.pr.tresorbackend.controller.UserController;
 import ch.bbw.pr.tresorbackend.repository.UserRepository;
 import ch.bbw.pr.tresorbackend.service.PasswordEncryptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.NoSuchAlgorithmException;
 
 public class OneTimeEncryptionScript {
     private final UserRepository userRepository;
     private final PasswordEncryptionService passwordEncryptionService;
+    private static final Logger logger = LoggerFactory.getLogger(OneTimeEncryptionScript.class);
 
     public OneTimeEncryptionScript(UserRepository userRepository, PasswordEncryptionService passwordEncryptionService) {
         this.userRepository = userRepository;
@@ -15,11 +19,15 @@ public class OneTimeEncryptionScript {
     }
 
     public void EncryptPasswords() throws NoSuchAlgorithmException {
-        var users = userRepository.findAll();
-        for(var user : users){
-            var hashedPassword = passwordEncryptionService.encryptPassword(user.getPassword());
-            user.setPassword(hashedPassword);
-            userRepository.save(user);
+        try{
+            var users = userRepository.findAll();
+            for(var user : users){
+                var hashedPassword = passwordEncryptionService.encryptPassword(user.getPassword());
+                user.setPassword(hashedPassword);
+                userRepository.save(user);
+            }
+        } catch(Exception e){
+            logger.error(e.getMessage());
         }
     }
 }
